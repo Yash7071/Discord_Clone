b572675886cb6411874077aa694f2ec074a8186b
YASH <yashaggarwal108@gmail.com>
2024-09-23 10:34:20 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  fontfamily:{
+    Whitney:["Whitney"],
+    Ginto:["Ginto"],
+    ggSans:["ggSans"],
+  },
+}
+
